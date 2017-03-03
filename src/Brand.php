571c638d8a04a51f86fdd5f90f36949b95d7886d
clayton collins <claytonccollins@gmail.com>
2017-03-03 11:14:53 -0800

    fail brand addstore test

--- src/Brand.php
+++ src/Brand.php
@@ -41,6 +41,28 @@ s<?php
             $this->id = $GLOBALS['DB']->lastInsertId();
         }
 
+        function addStore($store)
+        {
+            $GLOBALS['DB']->exec("INSERT INTO stores_brands (store_id, brand_id) VALUES ({$store->getId()}, {$this->getId()});");
+        }
+
+        function getStores()
+        {
+            $returned_stores = $GLOBALS['DB']->query("SELECT stores.* FROM brands JOIN stores_brands ON (brands.id = stores_brands.brand_id) JOIN stores ON (stores_brands.store_id = stores.id) WHERE brand.id = {$this->getId()};");
+            $stores = [];
+            foreach($returned_stores as $store)
+            {
+                $name = $store['name'];
+                $pricing = $store['pricing'];
+                $location = $store['location'];
+                $id = $store['id'];
+                $new_store = new Store($name, $pricing, $location, $id);
+                array_push($stores, $new_store);
+            }
+            return $stores;
+
+        }
+
         static function getAll()
         {
             $returned_brands = $GLOBALS['DB']->query("SELECT * FROM brands;");
--- tests/BrandTest.php
+++ tests/BrandTest.php
@@ -4,6 +4,7 @@
 * @backupStaticAttributes disabled
 */
 require_once "src/Brand.php";
+require_once "src/Store.php";
 
 $server = 'mysql:host=localhost:8889;dbname=shoes_test';
 $username = 'root';
@@ -12,7 +13,8 @@ $DB = new PDO($server, $username, $password);
 class BrandTest extends PHPUnit_Framework_TestCase{
     protected function teardown()
     {
-        // Brand::deleteAll();
+        Brand::deleteAll();
+        Store::deleteAll();
     }
     function test_getName() {
         $name = 'Spooky';
@@ -71,6 +73,35 @@ class BrandTest extends PHPUnit_Framework_TestCase{
         $result = Brand::getAll();
 
         $this->assertEquals([$test_brand, $test_brand2], $result);
+    }
+
+    function test_getStores() {
+        $name = 'Spooky';
+        $pricing = 9;
+        $test_brand = new Brand($name, $pricing);
+
+        $name2 = 'Acreepas';
+        $pricing2 = 4;
+        $test_brand2 = new Brand($name2, $pricing2);
+
+        $name = 'Doot Locker';
+        $pricing = 7;
+        $location = 'spooky dooty lane';
+        $test_store = new Store($name, $pricing, $location);
+
+        $name2 = 'Scareless Shoe Src';
+        $pricing2 = 4;
+        $location2 = '35 spook me';
+        $test_store2 = new Store($name2, $pricing2, $location2);
+
+        $test_brand->save();
+        $test_brand2->save();
+        $test_store->save();
+        $test_store2->save();
+        $test_brand->addStore($test_store2);
+        $result = $test_brand->getStores();
+
+        $this->assertEquals([$test_store2], $result)
     }
 
 }
